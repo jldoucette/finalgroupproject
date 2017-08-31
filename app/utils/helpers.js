842efa15ac4abdd113837f06db229e2ed35e9b07
jldoucette <jd@jonathandoucette.com>
2017-08-30 20:56:14 -0700

    DB routes

---
+++ app/utils/helpers.js
@@ -0,0 +1,27 @@
+var axios = require("axios");
+var bcrypt = require("bcryptjs");
+
+var helpers = {
+
+    runRetrieve: function() {
+        console.log("Running testRetrieve");
+        axios.get('/api/testRetrieve').then(function(results){
+            console.log("Ran testretrieve logging results below: ");
+            console.log(results);
+            return(results);
+        });
+    },
+
+    guestupdate: function() {
+        console.log("Running testRetrieve");
+        var testid='1';
+        axios.put('/api/updateguest').then(function(results){
+            console.log("Ran api/updateguest: ");
+            console.log(results);
+            return(results);
+        });
+    }
+};
+
+
+module.exports = helpers;
